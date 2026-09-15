332ce12a4b4652e149d9622521cb8920217244bf
mcodez72 <mikeymailbox@icloud.com>
2026-09-15 18:21:14 -0400

    MoodieApp

---
+++ moodie/MoodieApp.java
@@ -0,0 +1,17 @@
+package moodie;
+// -------------------------------------------------------------------------
+/**
+ *  Main class that runs the program and interacts with the user
+ * 
+ *  @author Mikey
+ *  @version Sep 15, 2026
+ */
+public class MoodieApp
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
