0936fdbd5cdc1e5e0b44fad494e51eef9217b13e
mcodez72 <mikeymailbox@icloud.com>
2026-09-18 13:32:35 -0400

    Everyones class

---
+++ moodie/MoodieMessage.java
@@ -0,0 +1,19 @@
+package moodie;
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author Emma
+ *  @version Sep 18, 2026
+ */
+public class MoodieMessage
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
