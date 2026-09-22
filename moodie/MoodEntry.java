6d2d0c8ae28508a4a28b9d4ff9aec1952a3e1a72
Emma Hartman <hartmane45@gmail.com>
2026-09-22 13:19:11 -0400

    Revise MoodEntry class documentation
    Updated class documentation to provide a concise summary.

--- moodie/MoodEntry.java
+++ moodie/MoodEntry.java
@@ -1,9 +1,7 @@
 package moodie;
 // -------------------------------------------------------------------------
 /**
- *  Write a one-sentence summary of your class here.
- *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ *  Holds the data for the mood entry
  * 
  *  @author Tia
  *  @version Sep 18, 2026
