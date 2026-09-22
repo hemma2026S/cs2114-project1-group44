64197162abb206c112e04eb5511705e59e3e99c2
Emma Hartman <hartmane45@gmail.com>
2026-09-22 13:21:03 -0400

    Clarify MoodMessage class purpose in comments
    Updated class documentation to specify mood message functionality.

--- moodie/MoodMessage.java
+++ moodie/MoodMessage.java
@@ -1,9 +1,7 @@
 package moodie;
 // -------------------------------------------------------------------------
 /**
- *  Write a one-sentence summary of your class here.
- *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ *  Gives the user a message depending on their mood rating
  * 
  *  @author Emma Hartman
  *  @version Sep 18, 2026
